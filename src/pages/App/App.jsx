299f4507d73e480faeab04870e4c0cc93c34cd9a
Ernest Bruno <erndobruno@gmail.com>
2022-02-04 11:24:35 -0800

    Remove unnecessary components in App and replace it with the ones I created. Also change the values of some of the color variables in index.css, and remove some css properties that I won't use.

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -3,17 +3,18 @@ import {useState} from 'react';
 import { Routes, Route } from 'react-router-dom';
 import {getUser} from './../../utilities/users-service';
 import NavBar from '../../components/NavBar/NavBar';
+import Splash from '../Splash/Splash';
+
 
 function App() {
   const [user, setUser] = useState(getUser());
   return (
     <main className="App">
-        <>
-            <NavBar setUser={setUser} user={user} /> 
-            <Routes>
-              {/* Routes will go here. */}
-            </Routes>
-        </>
+        <NavBar setUser={setUser} user={user} /> 
+        <Routes>
+          {/* Routes will go here. */}
+          <Route path="/" element={<Splash />} />
+        </Routes>
     </main>
   );
 }
