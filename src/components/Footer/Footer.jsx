146611d11b759093c071ad1aa1e5b69741668762
Ernest Bruno <erndobruno@gmail.com>
2022-02-04 11:21:17 -0800

    Add a Footer and set it at the bottom of the page.

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            Powered by Ernest
+        </div>
+    );
+};
+
+export default Footer;
