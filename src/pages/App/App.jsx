1f95879863807a43cfc786ea95c7354e39bdf3ac
Ernest Bruno <erndobruno@gmail.com>
2022-02-04 11:39:23 -0800

    Move NavBar and Footers to App, move about out of Splash into its own folder and import into Splash. Add some more global css properties.

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -2,8 +2,9 @@ import './App.css';
 import {useState} from 'react';
 import { Routes, Route } from 'react-router-dom';
 import {getUser} from './../../utilities/users-service';
-import NavBar from '../../components/NavBar/NavBar';
 import Splash from '../Splash/Splash';
+import NavBar from '../../components/NavBar/NavBar';
+import Footer from '../../components/Footer/Footer';
 
 
 function App() {
@@ -13,8 +14,9 @@ function App() {
         <NavBar setUser={setUser} user={user} /> 
         <Routes>
           {/* Routes will go here. */}
-          <Route path="/" element={<Splash />} />
+          <Route path="/" element={<Splash user={user} setUser={setUser} />} />
         </Routes>
+        <Footer/>
     </main>
   );
 }
