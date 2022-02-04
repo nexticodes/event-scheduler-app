8da9cb811c191ccef0a4f206018a9bc68d0309c7
Ernest Bruno <erndobruno@gmail.com>
2022-02-04 10:38:41 -0800

    Remove unnecessary pages and code. Starting somewhat fresh.

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -2,28 +2,18 @@ import './App.css';
 import {useState} from 'react';
 import { Routes, Route } from 'react-router-dom';
 import {getUser} from './../../utilities/users-service';
-import NewOrderPage from '../NewOrderPage/NewOrderPage';
-import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
-import AuthPage from '../AuthPage/AuthPage';
 import NavBar from '../../components/NavBar/NavBar';
 
 function App() {
   const [user, setUser] = useState(getUser());
   return (
     <main className="App">
-        <NavBar setUser={setUser} user={user} /> 
-        {user ? 
-          <>
-              <Routes>
-                {/* Routes will go here. */}
-                <Route path="/orders/new" element={<NewOrderPage />} />
-                <Route path="/orders" element={<OrderHistoryPage />} />
-                <Route path="/signup" element={<AuthPage setUser={setUser} />} />
-              </Routes>
-          </>
-          :
-          <AuthPage setUser={setUser} />
-        }
+        <>
+            <NavBar setUser={setUser} user={user} /> 
+            <Routes>
+              {/* Routes will go here. */}
+            </Routes>
+        </>
     </main>
   );
 }
