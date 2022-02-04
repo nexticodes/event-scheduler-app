f8d5abc6492fbd715a96896c488ee56fb1bff0fa
Ernest Bruno <erndobruno@gmail.com>
2022-02-04 11:20:19 -0800

    Create CreatorContainer containing information about creator of app.

---
+++ src/components/CreatorContainer/CreatorContainer.jsx
@@ -0,0 +1,12 @@
+import './CreatorContainer.css'
+
+const CreatorContainer = () => {
+    return (
+        <div className='creator-container'>
+            <img src='' alt='Photo of Ernest Bruno'></img>
+            <p>Hi, my name is Ernest Bruno</p>
+        </div>
+    );
+}
+
+export default CreatorContainer;
