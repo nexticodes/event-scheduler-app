d8412f3f67aa200da7958230b8c60210b7459eb2
Ernest Bruno <erndobruno@gmail.com>
2022-02-04 20:37:18 -0800

    Change number of events to display and edit events wrapper class.

--- src/pages/EventsPage/EventsPage.jsx
+++ src/pages/EventsPage/EventsPage.jsx
@@ -1,15 +1,21 @@
 import './EventsPage.css';
 import ProfileBox from '../../components/ProfileBox/ProfileBox';
 import ProfileDashboard from '../../components/ProfileDashboard/ProfileDashboard';
+import EventsList from '../../components/EventsList/EventsList';
 
 const EventsPage = ({user}) => {
+    const events = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
     return (
-        <>
+        <main className="events-page">
             <div className="user-profile-container">
                 <ProfileBox user={user} />
                 <ProfileDashboard user={user} />
             </div>
-        </>
+            <hr />
+            <div className="events-wrapper">
+                <EventsList events={events} user={user}/>
+            </div>
+        </main>
     )
 }
 
