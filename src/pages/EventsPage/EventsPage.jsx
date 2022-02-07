b3771d9da89cb7266d8ea29e69aa79dfbadc35d9
Ernest Bruno <erndobruno@gmail.com>
2022-02-07 10:31:44 -0800

    Add the modal.

--- src/pages/EventsPage/EventsPage.jsx
+++ src/pages/EventsPage/EventsPage.jsx
@@ -1,19 +1,23 @@
 import './EventsPage.css';
-import { Link } from 'react-router-dom';
 import ProfileBox from '../../components/ProfileBox/ProfileBox';
 import ProfileDashboard from '../../components/ProfileDashboard/ProfileDashboard';
 import EventsList from '../../components/EventsList/EventsList';
+import { useState } from 'react';
+import Modal from '../../components/Modal/Modal';
+
 
 const EventsPage = ({user}) => {
+    const [modalVisible, setModalVisible] = useState(false);
     const events = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
     return (
         <main className="events-page">
+            {modalVisible && <Modal user={user} setModalVisible={setModalVisible} />}
             <div className="user-profile-container">
                 <ProfileBox user={user} />
                 <ProfileDashboard user={user} />
             </div>
             <div className='events-options-container'>
-                <Link to='/events/add'><button>Add Event</button></Link>
+                <button onClick={() => setModalVisible(true)}>Add Event</button>
                 <button>Search Event</button>
             </div>
             <EventsList events={events} user={user}/>
