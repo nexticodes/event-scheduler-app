d13abe804a8c4186e2243dce356a53089d89ea83
Ernest Bruno <erndobruno@gmail.com>
2022-02-08 16:56:24 -0800

    Fix styling of input boxes when component is in update mode. Also change some of the input to textareas, and swap some of the places.

--- src/components/EventDetails/EventDetails.jsx
+++ src/components/EventDetails/EventDetails.jsx
@@ -19,9 +19,20 @@ const EventDetails = ({selectedEvent}) => {
     const [updatedEvent, setUpdatedEvent] = useState({
         ...selectedEvent
     })
+    const [chkbx, setChkbx] = useState(selectedEvent['active'])
     const [isUpdating, setIsUpdating] = useState(false);
     const handleChange = (e) => {
+        if (e.target.name === 'active'){ 
+            setChkbx(!chkbx);
+            setUpdatedEvent({...updatedEvent, 'active': !chkbx});
+            return;
+        }
         setUpdatedEvent({...updatedEvent, [e.target.name]: e.target.value});
+    }
+    const formatDate = (date) => {
+        let dateArray = date.toLocaleDateString().split(/\D/).slice(0,3).map(num=>num.padStart(2,"0"));
+
+        return [dateArray[2], dateArray[0], dateArray[1]].join("-");
     }
 
     return (
@@ -32,17 +43,14 @@ const EventDetails = ({selectedEvent}) => {
 
             <form className='event-details-form'>
                 <div className='top'>
-                    <div className='deets'>
-                            <input maxLength={3} disabled={!isUpdating} className='alias' name='alias' value={updatedEvent['alias']} onChange={handleChange}/>
-                        <span className='smaller'>
-                            ALIAS
-                        </span>
+                        <div className='deets'>
+                                <input maxLength={3} disabled={!isUpdating} className='alias' name='alias' value={updatedEvent['alias']} onChange={handleChange}/>
+                        </div>
 
-                    </div>
-                    <div className='datetime'>
-                        <input type='date' disabled={!isUpdating} className='eventDate' name='eventDate' value={new Date(updatedEvent['eventDate']).toLocaleString()} onChange={handleChange}/>
-                        <input type='time' disabled={!isUpdating} className='eventTime' name='eventTime' value={updatedEvent['eventTime']} onChange={handleChange}/>
-                    </div>
+                        <div className='datetime'>
+                            <input type='date' disabled={!isUpdating} className='eventDate' name='eventDate' value={formatDate(new Date(updatedEvent['eventDate']))} onChange={handleChange}/>
+                            <input type='time' disabled={!isUpdating} className='eventTime' name='eventTime' value={updatedEvent['eventTime']} onChange={handleChange}/>
+                        </div>
                 </div>
                 <div className='location'>
                     IMAGINARY MAP GOES HERE
@@ -50,17 +58,16 @@ const EventDetails = ({selectedEvent}) => {
                 </div>
                 <div className='bottom'>
                     <div className='bottom-numbers'>
-                        <span>
+                        <label>
                             <input disabled={!isUpdating} type='number' className='coverFee' name='coverFee' value={updatedEvent['coverFee']} onChange={handleChange}/>
 
-                        </span>
-                        <span>
-
+                        </label>
+                        <label>
                             <input disabled={!isUpdating} type='number' className='gracePeriod' name='gracePeriod' value={updatedEvent['gracePeriod']} onChange={handleChange}/>
-                        </span>
-                        <span>
+                        </label>
+                        <label>
                             <input disabled={!isUpdating} type='number' className='finalWarning' name='finalWarning' value={updatedEvent['finalWarning']} onChange={handleChange}/>
-                        </span>
+                        </label>
                         
                     </div>
                     <div className='bottom-active'>
@@ -68,7 +75,9 @@ const EventDetails = ({selectedEvent}) => {
                     <label>
                         Active
                     </label>
-                        <input type='checkbox' name='active' checked={updatedEvent.active === 'true'} onChange={handleChange}/>
+                        <input 
+                        disabled={!isUpdating}
+                        type='checkbox' name='active' checked={chkbx} onChange={handleChange}/>
                     </div>
                 </div>
             </form>
