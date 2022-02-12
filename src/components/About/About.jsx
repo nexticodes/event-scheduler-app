2d331d5d2c4c2bd781a5f4b49fe9567e45ea2988
Ernest Bruno <erndobruno@gmail.com>
2022-02-12 15:14:56 -0800

    Change styling and content of about page.

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,20 +6,21 @@ const About = () => {
     <div className="about-container">
       <div className="about-info">
         <div className="about-content">
-          <h3>A new way of getting your friends to commit to the plans you verbally set last week! <br/> Or yesterday! <br/> OR WHENEVER IT DOESN'T MATTER!</h3>
+          <h2 className='bangers-title'>A new way of getting your friends to commit to the plans you verbally set last week! <br/><br/> Or yesterday!</h2><br /><br />
+          <h2 className='bangers-title'>OR WHENEVER IT DOESN'T MATTER!</h2>
         </div>
       </div>
       <hr />
       <div className="about-info">
-        <div className="about-content">
-          <h3>Planning and committing made simple!</h3>
+        <div className="about-content ">
+          <h2 className='bangers-title'>Planning and committing made simple!</h2>
           <p>Invite your friends to your event through the app, and they will most likely commit to it!</p>
         </div>
       </div>
       <hr />
       <div className="about-info">
         <div className="about-content">
-          <h1>"HOW" you say?!</h1>
+          <h1 className='bangers-title'>"HOW" you say?!</h1>
           <button className='link-button'><Link to='/auth'>GET STARTED HERE</Link></button>
         </div>
       </div>
