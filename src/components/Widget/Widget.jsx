5151447b81988b0efc07af4d587e01c2738ba44c
Ernest Bruno <erndobruno@gmail.com>
2022-02-22 15:40:46 -0800

    Pass down userId and channelId.

--- src/components/Widget/Widget.jsx
+++ src/components/Widget/Widget.jsx
@@ -3,14 +3,14 @@ import {useState} from 'react';
 import {BsChatRightText} from 'react-icons/bs'
 import ChatBox from '../ChatBox/ChatBox';
 
-const Widget = ({channelId}) => {
+const Widget = ({userId, channelId}) => {
     const [clicked, setClicked] = useState(false);
     function handleX() {
         setClicked(!clicked);
     }
     return (
         <div className='widget-container' >
-            {clicked ? <ChatBox channelId={channelId} handleX={handleX}/> : <BsChatRightText onClick={() => setClicked(!clicked)}/>}
+            {clicked ? <ChatBox userId={userId} channelId={channelId} handleX={handleX}/> : <BsChatRightText onClick={() => setClicked(!clicked)}/>}
         </div>
     );
 }
