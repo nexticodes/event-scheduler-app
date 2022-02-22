e46fdd4a15ba9dc4fc32393be1a632811aad0763
Ernest Bruno <erndobruno@gmail.com>
2022-02-21 16:00:07 -0800

    Remove cursor pointer from chatbox container and place move it to just the SVG. Add an X in chatbox container.

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -1,7 +1,14 @@
 import './ChatBox.css';
 import {BiMailSend} from 'react-icons/bi'
+import { useState } from 'react';
 
 const ChatBox = () => {
+    const [message, setMessage] = useState('');
+    
+    function handleChange(e) {
+        setMessage(e.target.value);
+    }
+
     return (
         <div className='chatbox-container'>
             <h1>X</h1>
@@ -10,7 +17,7 @@ const ChatBox = () => {
             </div>
             <div className='chatbox-controls'>
                 <div className='chatbox-textarea'>
-                    <textarea></textarea>
+                    <textarea value={message} onChange={handleChange}></textarea>
                 </div>
                 <div className='chatbox-button'>
                     <BiMailSend/>
