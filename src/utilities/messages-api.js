94c053a3b1d682ebc0c38dc1b261db90d54e2737
Ernest Bruno <erndobruno@gmail.com>
2022-02-22 15:33:54 -0800

    Add a function that sends a message and adds it to the database.

--- src/utilities/messages-api.js
+++ src/utilities/messages-api.js
@@ -1,3 +1,7 @@
 import {sendRequest} from './users-api';
 
 const BASE_URL = '/api/messages';
+
+export function sendMessage(channelId, messageData){
+    return sendRequest(`${BASE_URL}/${channelId}`, 'POST', messageData)
+}
