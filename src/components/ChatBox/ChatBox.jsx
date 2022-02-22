106fe95a3f83aeb6e991a441954edcd94ed3bce7
Ernest Bruno <erndobruno@gmail.com>
2022-02-21 16:27:52 -0800

    Change div to button and apply style to it.

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -2,27 +2,32 @@ import './ChatBox.css';
 import {BiMailSend} from 'react-icons/bi'
 import { useState } from 'react';
 
-const ChatBox = () => {
+const ChatBox = ({handleX}) => {
     const [message, setMessage] = useState('');
     
     function handleChange(e) {
         setMessage(e.target.value);
     }
 
+    function handleSubmit(e){
+        e.preventDefault();
+        console.log('Yo');
+    }
+
     return (
         <div className='chatbox-container'>
-            <h1>X</h1>
+            <h1 onClick={handleX}>X</h1>
             <div className='chatbox-messages'>
 
             </div>
-            <div className='chatbox-controls'>
+            <form className='chatbox-controls'>
                 <div className='chatbox-textarea'>
                     <textarea value={message} onChange={handleChange}></textarea>
                 </div>
-                <div className='chatbox-button'>
-                    <BiMailSend/>
-                </div>
-            </div>
+                <button id='chatbox-button' onClick={handleSubmit}>
+                    <BiMailSend />
+                </button>
+            </form>
         </div>
     )
 }
--- src/components/Widget/Widget.jsx
+++ src/components/Widget/Widget.jsx
@@ -5,9 +5,12 @@ import ChatBox from '../ChatBox/ChatBox';
 
 const Widget = () => {
     const [clicked, setClicked] = useState(false);
+    function handleX() {
+        setClicked(!clicked);
+    }
     return (
         <div className='widget-container' >
-            {clicked ? <ChatBox/> : <BsChatRightText onClick={() => setClicked(!clicked)}/>}
+            {clicked ? <ChatBox handleX={handleX}/> : <BsChatRightText onClick={() => setClicked(!clicked)}/>}
         </div>
     );
 }
