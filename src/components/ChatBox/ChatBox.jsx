052f644b2e82e856daad19180c3ca5527d9ccbd9
Ernest Bruno <erndobruno@gmail.com>
2022-02-23 14:27:41 -0800

    Attach socket to chatbox.

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -2,28 +2,36 @@ import './ChatBox.css';
 import {BiMailSend} from 'react-icons/bi'
 import { useState, useEffect } from 'react';
 import * as messagesAPI from '../../utilities/messages-api';
+
 import ChatMessage from '../ChatMessage/ChatMessage';
 
-const ChatBox = ({userId, channelId, handleX}) => {
+
+const ChatBox = ({socket, userId, channelId, handleX}) => {
     const [message, setMessage] = useState('');
     const [allMessages, setAllMessages] = useState([]);
 
-
     useEffect(() => {
-        async function getMessages(){
-            const channelMessages = await messagesAPI.getMessages(channelId);
-            setAllMessages(channelMessages);
-        }
         getMessages();
-    }, [])
+        socket.on('receive-message', (data) => {
+            getMessages();
+        })
+    }, [socket])
 
     function handleChange(e) {
         setMessage(e.target.value);
     }
 
+    async function getMessages(){
+        const channelMessages = await messagesAPI.getMessages(channelId);
+        setAllMessages(channelMessages);
+    }
+
     async function handleSubmit(e){
         e.preventDefault();
-        const newMessage = await messagesAPI.sendMessage(channelId, {message, userId})
+        const newMessage = await messagesAPI.sendMessage(channelId, {message, userId});
+        await socket.emit('send-message', {message, userId, channelId})
+        getMessages();
+        setMessage('');
     }
 
     return (
